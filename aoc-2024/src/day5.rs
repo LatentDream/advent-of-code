3c4f604f2118762060fa06113f284e97081025cd
Latent <guillaume.thibault.98@proton.me>
2024-12-05 19:51:45 -0500

    [2024] Day 5 - Part 2

--- aoc-2024/src/day5.rs
+++ aoc-2024/src/day5.rs
@@ -5,6 +5,7 @@ use std::{
 };
 
 type Rules = HashMap<i32, Vec<i32>>;
+type Update = Vec<i32>;
 type Updates = Vec<Vec<i32>>;
 
 fn get_input() -> (Rules, Updates) {
@@ -39,7 +40,7 @@ fn get_input() -> (Rules, Updates) {
     (rules, inputs)
 }
 
-pub fn inverse_rules(rules_priority: &Rules) -> Rules {
+pub fn get_depends_on_from_priority_rules(rules_priority: &Rules) -> Rules {
     let mut inverted_rules: Rules = HashMap::new();
 
     for (key, values) in rules_priority.iter() {
@@ -54,12 +55,67 @@ pub fn inverse_rules(rules_priority: &Rules) -> Rules {
     inverted_rules
 }
 
+pub fn sum_updates(updates: Updates) -> i32 {
+    let mut total_valid = 0;
+    for update in updates {
+        if update.len() % 2 != 1 {
+            panic!("Is valid {:?}, but len is pair", update);
+        }
+        total_valid += update[update.len() / 2];
+    }
+    total_valid
+}
+
+pub fn fix_update(update: &mut Update, rules_dependencies: &Rules) {
+    let len = update.len();
+    println!("\ninput update {:?}", update);
+    let set: HashSet<i32> = update.iter().cloned().collect();
+
+    // Sort by number of dependencies and value (as tiebreaker)
+    update.sort_by(|&a, &b| {
+        let a_deps = rules_dependencies
+            .get(&a)
+            .unwrap_or(&Vec::new())
+            .iter()
+            .filter(|&&x| set.contains(&x))
+            .count();
+        let b_deps = rules_dependencies
+            .get(&b)
+            .unwrap_or(&Vec::new())
+            .iter()
+            .filter(|&&x| set.contains(&x))
+            .count();
+
+        // If same number of dependencies, sort by value
+        if a_deps == b_deps {
+            b.cmp(&a)
+        } else {
+            a_deps.cmp(&b_deps)
+        }
+    });
+
+    // Bubble up elements that violate dependencies
+    for i in 0..len {
+        for j in (i + 1)..len {
+            if let Some(deps) = rules_dependencies.get(&update[i]) {
+                if deps.contains(&update[j]) && set.contains(&update[j]) {
+                    update.swap(i, j);
+                }
+            }
+        }
+    }
+
+    println!("Sorted update {:?}", update);
+}
+
 pub fn solve() {
     let (rules_priority, updates) = get_input();
-    let rules_depends_on = inverse_rules(&rules_priority);
+    let rules_depends_on = get_depends_on_from_priority_rules(&rules_priority);
 
     let mut valid_updates: Updates = Vec::new();
+    let mut invalid_updates: Updates = Vec::new();
 
+    // Sort Valid | Invalid
     'update_loop: for update in updates {
         let mut seens: HashSet<i32> = HashSet::with_capacity(update.len());
         let mut need_update: HashSet<i32> = update.iter().cloned().collect();
@@ -73,6 +129,7 @@ pub fn solve() {
             if let Some(deps) = page_deps {
                 for dep in deps {
                     if need_update.contains(&dep) {
+                        invalid_updates.push(update);
                         continue 'update_loop;
                     }
                 }
@@ -86,13 +143,13 @@ pub fn solve() {
     }
     println!("\n{:?}", valid_updates);
 
-    let mut total_p1 = 0;
-    for update in valid_updates {
-        if update.len() % 2 != 1 {
-            panic!("Is valid {:?}, but len is pair", update);
-        }
-        total_p1 += update[update.len() / 2];
-    }
+    // Sum valid input
+    println!("Solution Part 1: {}", sum_updates(valid_updates));
 
-    println!("Solution {}", total_p1);
+    // Fix the invalid input
+    println!("Rules: {:?}", rules_priority);
+    for update in &mut invalid_updates {
+        fix_update(update, &rules_depends_on);
+    }
+    println!("Solution Part 1: {}", sum_updates(invalid_updates));
 }
