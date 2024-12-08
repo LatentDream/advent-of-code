0cf79b25955831dc79d3989f63f58f5259a027ca
Latent <guillaume.thibault.98@proton.me>
2024-12-08 15:46:09 -0500

    [2024] Day 8

---
+++ aoc-2024/src/day8.rs
@@ -0,0 +1,144 @@
+use std::{
+    collections::{HashMap, HashSet},
+    fs::read_to_string,
+    hash::{Hash, Hasher},
+};
+
+type Frequency = char;
+#[derive(Clone, Debug)]
+struct Coord {
+    x: i32,
+    y: i32,
+}
+
+impl PartialEq for Coord {
+    fn eq(&self, other: &Coord) -> bool {
+        self.x.eq(&other.x) && self.y.eq(&other.y)
+    }
+}
+impl Eq for Coord {}
+impl Hash for Coord {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.x.hash(state);
+        self.y.hash(state);
+    }
+}
+
+struct CityMap {
+    map: Vec<Vec<char>>,
+    max_x: i32,
+    max_y: i32,
+}
+
+pub fn build_city_map(input: String) -> CityMap {
+    let map: Vec<Vec<char>> = input
+        .split("\n")
+        .filter(|line| !line.trim().is_empty())
+        .map(|line| line.chars().collect())
+        .collect();
+
+    let max_y = map.len() as i32;
+    let max_x = map[0].len() as i32;
+
+    CityMap { map, max_x, max_y }
+}
+
+fn find_antennas(city_map: &CityMap) -> HashMap<Frequency, HashSet<Coord>> {
+    let mut antennas: HashMap<Frequency, HashSet<Coord>> = HashMap::new();
+    for (y, line) in city_map.map.iter().enumerate() {
+        for (x, c) in line.iter().enumerate() {
+            if *c != '.' {
+                let coord = Coord {
+                    x: x.try_into().unwrap(),
+                    y: y.try_into().unwrap(),
+                };
+                antennas
+                    .entry(*c)
+                    .or_insert_with(|| HashSet::new())
+                    .insert(coord);
+            }
+        }
+    }
+    antennas
+}
+
+type AntinodesFinder = fn(&Coord, &Coord, &CityMap) ->  Vec<Coord> ;
+
+fn find_antinodes_part1(antenna_1: &Coord, antenna_2: &Coord, city_map: &CityMap) -> Vec<Coord> {
+    let (delta_x, delta_y) = (antenna_2.x - antenna_1.x, antenna_2.y - antenna_1.y);
+    let antinodes = vec![
+        Coord { x: antenna_2.x + delta_x, y: antenna_2.y + delta_y },
+        Coord { x: antenna_1.x - delta_x, y: antenna_1.y - delta_y },
+    ];
+    antinodes
+        .into_iter()
+        .filter(|coord| coord.x >= 0 && coord.x < city_map.max_x && coord.y >= 0 && coord.y < city_map.max_y)
+        .collect()
+}
+
+fn find_antinodes_part2(antenna_1: &Coord, antenna_2: &Coord, city_map: &CityMap) -> Vec<Coord> {
+    let (delta_x, delta_y) = (antenna_2.x - antenna_1.x, antenna_2.y - antenna_1.y);
+    let mut antinodes = vec![antenna_1.clone(), antenna_2.clone()];
+    
+    let mut current = Coord { x: antenna_2.x, y: antenna_2.y };
+    loop {
+        current.x += delta_x;
+        current.y += delta_y;
+        
+        // Check if we're still within bounds
+        if current.x < 0 || current.x >= city_map.max_x || 
+           current.y < 0 || current.y >= city_map.max_y {
+            break;
+        }
+        
+        antinodes.push(current.clone());
+    }
+    
+    let mut current = Coord { x: antenna_1.x, y: antenna_1.y };
+    loop {
+        current.x -= delta_x;
+        current.y -= delta_y;
+        
+        // Check if we're still within bounds
+        if current.x < 0 || current.x >= city_map.max_x || 
+           current.y < 0 || current.y >= city_map.max_y {
+            break;
+        }
+        
+        antinodes.push(current.clone());
+    }
+    
+    antinodes
+}
+
+fn find_antinodes_for_freq(same_freq_antennas: &HashSet<Coord>, city_map: &CityMap, finder: AntinodesFinder) -> HashSet<Coord> {
+    let mut antinodes_freq = HashSet::new();
+
+    for coord1 in same_freq_antennas {
+        for coord2 in same_freq_antennas {
+            if coord1 != coord2 {
+                let ans = finder(coord1, coord2, city_map);
+                antinodes_freq.extend(ans);
+            }
+        }
+    }
+
+    antinodes_freq
+}
+
+pub fn solve() {
+    let input = read_to_string("input.txt").expect("the file to open");
+    let city_map = build_city_map(input);
+    let antennas = find_antennas(&city_map);
+
+    let mut antinodes_part1 = HashSet::new();
+    let mut antinodes_part2 = HashSet::new();
+    for (_freq, same_freq_antennas) in antennas {
+        antinodes_part1.extend(find_antinodes_for_freq(&same_freq_antennas, &city_map, find_antinodes_part1)); 
+        antinodes_part2.extend(find_antinodes_for_freq(&same_freq_antennas, &city_map, find_antinodes_part2));
+    }
+    
+    println!("Part 1: {}", antinodes_part1.len());
+    println!("Part 1: {}", antinodes_part2.len());
+
+}
--- aoc-2024/src/main.rs
+++ aoc-2024/src/main.rs
@@ -4,8 +4,9 @@
 // mod day4;
 // mod day5;
 // mod day6;
-mod day7;
+// mod day7;
+mod day8;
 
 fn main() {
-    day7::solve();
+    day8::solve();
 }
