eb666a27cdc8be22b1b239b50c557c49cc7861e3
Latent <guillaume.thibault.98@proton.me>
2024-12-14 13:12:49 -0500

    [2024] Day 14 | Part 2 (with grep)

--- aoc-2024/src/day14.rs
+++ aoc-2024/src/day14.rs
@@ -1,43 +1,42 @@
 use aoc_2024::utils::Coord;
 use std::fs::read_to_string;
+use std::io::{self, Write};
 
 const MAP_WIDTH: i32 = 101;
 const MAP_HEIGHT: i32 = 103;
 
 #[derive(Debug)]
-struct Robot {
+pub struct Robot {
     location: Coord,
     velocity: Coord,
 }
 
 pub fn solve() {
     let input = read_to_string("input.txt").expect("the file to open");
+
+    // Part 1
     let mut robots = parse_input(&input);
     for _ in 0..100 {
         for robot in robots.iter_mut() {
             robot.r#move();
         }
     }
-    // debug_robots(&robots);
+    println!(
+        "Product of robots in quadrants: {}",
+        cound_robot_by_cadran(&robots)
+    );
 
-    let mut cadran = [0, 0, 0, 0];
-    let middle_x = MAP_WIDTH / 2;
-    let middle_y = MAP_HEIGHT / 2;
+    // # For part 2
+    // cargo run > output.txt
+    // rg -A 100 -B 100 \"11111111111111111\"
+    for i in 0..10000 {
+        println!("Iteration number: {}", i + 100);
+        debug_robots(&robots);
 
-    for robot in &robots {
-        let x = robot.location.x;
-        let y = robot.location.y;
-
-        match (x.cmp(&middle_x), y.cmp(&middle_y)) {
-            (std::cmp::Ordering::Greater, std::cmp::Ordering::Less) => cadran[0] += 1,  // Top-right
-            (std::cmp::Ordering::Less, std::cmp::Ordering::Less) => cadran[1] += 1,     // Top-left
-            (std::cmp::Ordering::Less, std::cmp::Ordering::Greater) => cadran[2] += 1,  // Bottom-left
-            (std::cmp::Ordering::Greater, std::cmp::Ordering::Greater) => cadran[3] += 1, // Bottom-right
-            _ => {} // On the dividing lines, not counted in any quadrant
+        for robot in robots.iter_mut() {
+            robot.r#move();
         }
     }
-
-    println!("Product of robots in quadrants: {}", cadran[0] * cadran[1] * cadran[2] * cadran[3]);
 }
 
 impl Robot {
@@ -74,6 +73,25 @@ fn parse_input(input: &str) -> Vec<Robot> {
         .collect()
 }
 
+fn cound_robot_by_cadran(robots: &[Robot]) -> i32 {
+    let mut cadran = [0, 0, 0, 0];
+    let middle_x = MAP_WIDTH / 2;
+    let middle_y = MAP_HEIGHT / 2;
+
+    for robot in robots {
+        let x = robot.location.x;
+        let y = robot.location.y;
+
+        match (x.cmp(&middle_x), y.cmp(&middle_y)) {
+            (std::cmp::Ordering::Greater, std::cmp::Ordering::Less) => cadran[0] += 1, // Top-right
+            (std::cmp::Ordering::Less, std::cmp::Ordering::Less) => cadran[1] += 1,    // Top-left
+            (std::cmp::Ordering::Less, std::cmp::Ordering::Greater) => cadran[2] += 1, // Bottom-left
+            (std::cmp::Ordering::Greater, std::cmp::Ordering::Greater) => cadran[3] += 1, // Bottom-right
+            _ => {} // On the dividing lines, not counted in any quadrant
+        }
+    }
+    cadran[0] * cadran[1] * cadran[2] * cadran[3]
+}
 
 pub fn debug_robots(robots: &[Robot]) {
     let mut map = vec![vec![0; MAP_WIDTH as usize]; MAP_HEIGHT as usize];
