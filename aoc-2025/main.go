79f8072755fed7a5f6b88c0637afbc37c635f687
Guillaume Thibault <guillaume.thibault@unity3d.com>
2025-12-03 23:29:04 -0500

    [2025] Day 3 | Part 2

--- aoc-2025/main.go
+++ aoc-2025/main.go
@@ -22,35 +22,9 @@ func day3(content string) {
 
 	totalPart1 := part1(input)
 	fmt.Printf("Part 1: %d", totalPart1)
-}
-// ANSI color codes for gradient effect
-func getColorCode(digit int) string {
-	colors := []string{
-		"\033[90m", // 0 - dark gray
-		"\033[94m", // 1 - dark blue
-		"\033[96m", // 2 - cyan
-		"\033[92m", // 3 - green
-		"\033[93m", // 4 - yellow
-		"\033[33m", // 5 - dark yellow
-		"\033[91m", // 6 - light red
-		"\033[31m", // 7 - red
-		"\033[95m", // 8 - magenta
-		"\033[97m", // 9 - bright white
-	}
-	return colors[digit]
-}
 
-func resetColor() string {
-	return "\033[0m"
-}
-
-func printColoredBank(bank string) {
-	fmt.Print("Bank: ")
-	for _, char := range bank {
-		digit, _ := strconv.Atoi(string(char))
-		fmt.Printf("%s%c%s", getColorCode(digit), char, resetColor())
-	}
-	fmt.Println()
+	totalPart2 := part2(input)
+	fmt.Printf("Part 2: %d", totalPart2)
 }
 
 func part1(banks []string) int {
@@ -85,6 +59,189 @@ func part1(banks []string) int {
 		fmt.Println("joltage: ", joltage)
 		total += int(joltageInt)
 
+	}
+	return total
+}
+
+func part2(banks []string) int {
+	total := 0
+	for _, bank := range banks {
+		if len(bank) == 0 {
+			continue
+		}
+		digit1, _ := strconv.Atoi(string(bank[0]))
+		digit2, _ := strconv.Atoi(string(bank[1]))
+		digit3, _ := strconv.Atoi(string(bank[2]))
+		digit4, _ := strconv.Atoi(string(bank[3]))
+		digit5, _ := strconv.Atoi(string(bank[4]))
+		digit6, _ := strconv.Atoi(string(bank[5]))
+		digit7, _ := strconv.Atoi(string(bank[6]))
+		digit8, _ := strconv.Atoi(string(bank[7]))
+		digit9, _ := strconv.Atoi(string(bank[8]))
+		digit10, _ := strconv.Atoi(string(bank[9]))
+		digit11, _ := strconv.Atoi(string(bank[10]))
+		digit12, _ := strconv.Atoi(string(bank[11]))
+
+		// Track positions where each digit was found
+		pos1, pos2, pos3, pos4, pos5, pos6 := 0, 1, 2, 3, 4, 5
+		pos7, pos8, pos9, pos10, pos11 := 6, 7, 8, 9, 10
+
+		for i, batterie := range bank {
+			if i < 1 {
+				continue
+			}
+
+			batterieInt, _ := strconv.Atoi(string(batterie))
+			
+			if batterieInt > digit1 && i <= len(bank)-12 {
+				digit1, _ = strconv.Atoi(string(bank[i]))
+				digit2, _ = strconv.Atoi(string(bank[i+1]))
+				digit3, _ = strconv.Atoi(string(bank[i+2]))
+				digit4, _ = strconv.Atoi(string(bank[i+3]))
+				digit5, _ = strconv.Atoi(string(bank[i+4]))
+				digit6, _ = strconv.Atoi(string(bank[i+5]))
+				digit7, _ = strconv.Atoi(string(bank[i+6]))
+				digit8, _ = strconv.Atoi(string(bank[i+7]))
+				digit9, _ = strconv.Atoi(string(bank[i+8]))
+				digit10, _ = strconv.Atoi(string(bank[i+9]))
+				digit11, _ = strconv.Atoi(string(bank[i+10]))
+				digit12, _ = strconv.Atoi(string(bank[i+11]))
+				pos1, pos2, pos3, pos4, pos5, pos6 = i, i+1, i+2, i+3, i+4, i+5
+				pos7, pos8, pos9, pos10, pos11 = i+6, i+7, i+8, i+9, i+10
+				continue
+			}
+
+			if batterieInt > digit2 && i <= len(bank)-11 && i > pos1 {
+				digit2, _ = strconv.Atoi(string(bank[i]))
+				digit3, _ = strconv.Atoi(string(bank[i+1]))
+				digit4, _ = strconv.Atoi(string(bank[i+2]))
+				digit5, _ = strconv.Atoi(string(bank[i+3]))
+				digit6, _ = strconv.Atoi(string(bank[i+4]))
+				digit7, _ = strconv.Atoi(string(bank[i+5]))
+				digit8, _ = strconv.Atoi(string(bank[i+6]))
+				digit9, _ = strconv.Atoi(string(bank[i+7]))
+				digit10, _ = strconv.Atoi(string(bank[i+8]))
+				digit11, _ = strconv.Atoi(string(bank[i+9]))
+				digit12, _ = strconv.Atoi(string(bank[i+10]))
+				pos2, pos3, pos4, pos5, pos6 = i, i+1, i+2, i+3, i+4
+				pos7, pos8, pos9, pos10, pos11 = i+5, i+6, i+7, i+8, i+9
+				continue
+			}
+
+			if batterieInt > digit3 && i <= len(bank)-10 && i > pos2 {
+				digit3, _ = strconv.Atoi(string(bank[i]))
+				digit4, _ = strconv.Atoi(string(bank[i+1]))
+				digit5, _ = strconv.Atoi(string(bank[i+2]))
+				digit6, _ = strconv.Atoi(string(bank[i+3]))
+				digit7, _ = strconv.Atoi(string(bank[i+4]))
+				digit8, _ = strconv.Atoi(string(bank[i+5]))
+				digit9, _ = strconv.Atoi(string(bank[i+6]))
+				digit10, _ = strconv.Atoi(string(bank[i+7]))
+				digit11, _ = strconv.Atoi(string(bank[i+8]))
+				digit12, _ = strconv.Atoi(string(bank[i+9]))
+				pos3, pos4, pos5, pos6 = i, i+1, i+2, i+3
+				pos7, pos8, pos9, pos10, pos11 = i+4, i+5, i+6, i+7, i+8
+				continue
+			}
+
+			if batterieInt > digit4 && i <= len(bank)-9 && i > pos3 {
+				digit4, _ = strconv.Atoi(string(bank[i]))
+				digit5, _ = strconv.Atoi(string(bank[i+1]))
+				digit6, _ = strconv.Atoi(string(bank[i+2]))
+				digit7, _ = strconv.Atoi(string(bank[i+3]))
+				digit8, _ = strconv.Atoi(string(bank[i+4]))
+				digit9, _ = strconv.Atoi(string(bank[i+5]))
+				digit10, _ = strconv.Atoi(string(bank[i+6]))
+				digit11, _ = strconv.Atoi(string(bank[i+7]))
+				digit12, _ = strconv.Atoi(string(bank[i+8]))
+				pos4, pos5, pos6, pos7 = i, i+1, i+2, i+3
+				pos8, pos9, pos10, pos11 = i+4, i+5, i+6, i+7
+				continue
+			}
+
+			if batterieInt > digit5 && i <= len(bank)-8 && i > pos4 {
+				digit5, _ = strconv.Atoi(string(bank[i]))
+				digit6, _ = strconv.Atoi(string(bank[i+1]))
+				digit7, _ = strconv.Atoi(string(bank[i+2]))
+				digit8, _ = strconv.Atoi(string(bank[i+3]))
+				digit9, _ = strconv.Atoi(string(bank[i+4]))
+				digit10, _ = strconv.Atoi(string(bank[i+5]))
+				digit11, _ = strconv.Atoi(string(bank[i+6]))
+				digit12, _ = strconv.Atoi(string(bank[i+7]))
+				pos5, pos6, pos7, pos8 = i, i+1, i+2, i+3
+				pos9, pos10, pos11 = i+4, i+5, i+6
+				continue
+			}
+
+			if batterieInt > digit6 && i <= len(bank)-7 && i > pos5 {
+				digit6, _ = strconv.Atoi(string(bank[i]))
+				digit7, _ = strconv.Atoi(string(bank[i+1]))
+				digit8, _ = strconv.Atoi(string(bank[i+2]))
+				digit9, _ = strconv.Atoi(string(bank[i+3]))
+				digit10, _ = strconv.Atoi(string(bank[i+4]))
+				digit11, _ = strconv.Atoi(string(bank[i+5]))
+				digit12, _ = strconv.Atoi(string(bank[i+6]))
+				pos6, pos7, pos8, pos9 = i, i+1, i+2, i+3
+				pos10, pos11 = i+4, i+5
+				continue
+			}
+
+			if batterieInt > digit7 && i <= len(bank)-6 && i > pos6 {
+				digit7, _ = strconv.Atoi(string(bank[i]))
+				digit8, _ = strconv.Atoi(string(bank[i+1]))
+				digit9, _ = strconv.Atoi(string(bank[i+2]))
+				digit10, _ = strconv.Atoi(string(bank[i+3]))
+				digit11, _ = strconv.Atoi(string(bank[i+4]))
+				digit12, _ = strconv.Atoi(string(bank[i+5]))
+				pos7, pos8, pos9 = i, i+1, i+2
+				pos10, pos11 = i+3, i+4
+				continue
+			}
+
+			if batterieInt > digit8 && i <= len(bank)-5 && i > pos7 {
+				digit8, _ = strconv.Atoi(string(bank[i]))
+				digit9, _ = strconv.Atoi(string(bank[i+1]))
+				digit10, _ = strconv.Atoi(string(bank[i+2]))
+				digit11, _ = strconv.Atoi(string(bank[i+3]))
+				digit12, _ = strconv.Atoi(string(bank[i+4]))
+				pos8, pos9, pos10, pos11 = i, i+1, i+2, i+3
+				continue
+			}
+
+			if batterieInt > digit9 && i <= len(bank)-4 && i > pos8 {
+				digit9, _ = strconv.Atoi(string(bank[i]))
+				digit10, _ = strconv.Atoi(string(bank[i+1]))
+				digit11, _ = strconv.Atoi(string(bank[i+2]))
+				digit12, _ = strconv.Atoi(string(bank[i+3]))
+				pos9, pos10, pos11 = i, i+1, i+2
+				continue
+			}
+
+			if batterieInt > digit10 && i <= len(bank)-3 && i > pos9 {
+				digit10, _ = strconv.Atoi(string(bank[i]))
+				digit11, _ = strconv.Atoi(string(bank[i+1]))
+				digit12, _ = strconv.Atoi(string(bank[i+2]))
+				pos10, pos11 = i, i+1
+				continue
+			}
+
+			if batterieInt > digit11 && i <= len(bank)-2 && i > pos10 {
+				digit11, _ = strconv.Atoi(string(bank[i]))
+				digit12, _ = strconv.Atoi(string(bank[i+1]))
+				pos11 = i
+				continue
+			}
+
+			if batterieInt > digit12 && i > pos11 {
+				digit12 = batterieInt
+			}
+		}
+		
+		joltage := fmt.Sprintf("%d%d%d%d%d%d%d%d%d%d%d%d", digit1, digit2, digit3, digit4, digit5, digit6, digit7, digit8, digit9, digit10, digit11, digit12)
+		joltageInt, _ := strconv.Atoi(joltage)
+
+		fmt.Println("joltage: ", joltage)
+		total += joltageInt
 	}
 	return total
 }
--- aoc-2025/main_test.go
+++ aoc-2025/main_test.go
@@ -35,3 +35,49 @@ func Test_part1(t *testing.T) {
 		})
 	}
 }
+
+func Test_part2(t *testing.T) {
+	tests := []struct {
+		name string
+		banks []string
+		want  int
+	}{
+		{
+			name: "Part 1 - Example",
+			banks: []string{
+				"987654321111111",
+			},
+			want: 987654321111,
+		},
+		{
+			name: "Part 1 - Example",
+			banks: []string{
+				"811111111111119",
+			},
+			want: 811111111119,
+		},
+		{
+			name: "Part 1 - Example",
+			banks: []string{
+				"234234234234278",
+			},
+			want: 434234234278,
+		},
+		{
+			name: "Part 1 - Example",
+			banks: []string{
+				"818181911112111",
+			},
+			want: 888911112111,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := part2(tt.banks)
+			if got != tt.want {
+				t.Errorf("part2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
